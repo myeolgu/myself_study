6fb7c7acb3efe1298f8d0a05ac023a5be24f5990
leejuyeop <juyeop198@etribe.co.kr>
2023-02-14 15:52:40 +0900

    1st-commit

---
+++ js/front.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    let swiper = new Swiper(".mySwiper", {
+      autoplay: {
+      delay:3000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+  });
+    let swiper2 = new Swiper(".mySwiper2", {
+      slidesPerView: 1,
+      autoplay: {
+      delay:3000,
+    },
+      grid: {
+      rows: 2,
+    },
+      loop: false,
+      spaceBetween: 30,
+      pagination: {	
+      el: ".swiper-pagination2",	
+			clickable: true,	
+			type : 'bullets',	
+		  },
+  });
+    let swiper3 = new Swiper(".mySwiper3", {
+      initialSlide: 1,
+      autoplay: {
+        delay:3000,
+      },
+      slidesPerView: "auto",
+      spaceBetween: 30,
+    });
+    let swiper4 = new Swiper(".mySwiper4", {
+      initialSlide: 1,
+      slidesPerView: 3,
+      slidesPerView: "auto",
+      spaceBetween: 30,
+    });
+    // toggle로 on off 
+    let toggle = 0;
+    $('.container .footer-bottom-last img').on("click", function() {
+      if (toggle == 0 ) {
+        toggle = 1;
+        $(".container .footer-bottom-last img").css({ transform : "rotate(0)" });
+        $(".container .footer-bottom-toggle").slideDown().css({ display : "block", "margin-top" : "20px" });
+
+      } else {
+        toggle = 0;
+        $(".container .footer-bottom-last img").css({ transform : "rotate(-180deg)" });
+        $(".container .footer-bottom-toggle").slideUp().css({ display : "none" });
+      }
+    });
+});
