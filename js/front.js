fa757aa17f3241ac9225f3ef735ce4dfe976cc3b
leejuyeop <juyeop198@etribe.co.kr>
2023-02-15 14:17:21 +0900

    gogogogo

--- js/front.js
+++ js/front.js
@@ -1,55 +1,55 @@
 $(document).ready(function () {
-    let swiper = new Swiper(".swiper-event-1", {
-      autoplay: {
-      delay:3000,
-    },
+  let swiper = new Swiper(".swiper-event-1", {
+    autoplay: {
+    delay:3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+});
+  let swiper2 = new Swiper(".swiper-event-2", {
+    slidesPerView: 1,
+    autoplay: {
+    delay:3000,
+  },
+    grid: {
+    rows: 2,
+  },
+    loop: false,
+    spaceBetween: 30,
     pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
+    el: ".swiper-pagination2",
+    clickable: true,
+    type : 'bullets',
     },
-  });
-    let swiper2 = new Swiper(".mySwiper2", {
-      slidesPerView: 1,
-      autoplay: {
+});
+  let swiper3 = new Swiper(".mySwiper3", {
+    initialSlide: 1,
+    autoplay: {
       delay:3000,
     },
-      grid: {
-      rows: 2,
-    },
-      loop: false,
-      spaceBetween: 30,
-      pagination: {	
-      el: ".swiper-pagination2",	
-			clickable: true,	
-			type : 'bullets',	
-		  },
+    slidesPerView: "auto",
+    spaceBetween: 30,
   });
-    let swiper3 = new Swiper(".mySwiper3", {
-      initialSlide: 1,
-      autoplay: {
-        delay:3000,
-      },
-      slidesPerView: "auto",
-      spaceBetween: 30,
-    });
-    let swiper4 = new Swiper(".mySwiper4", {
-      initialSlide: 1,
-      slidesPerView: 3,
-      slidesPerView: "auto",
-      spaceBetween: 30,
-    });
-    // toggle로 on off 
-    let toggle = 0;
-    $('.container .footer-bottom-last img').on("click", function() {
-      if (toggle == 0 ) {
-        toggle = 1;
-        $(".container .footer-bottom-last img").css({ transform : "rotate(0)" });
-        $(".container .footer-bottom-toggle").slideDown().css({ display : "block", "margin-top" : "20px" });
+  let swiper4 = new Swiper(".mySwiper4", {
+    initialSlide: 1,
+    slidesPerView: 3,
+    slidesPerView: "auto",
+    spaceBetween: 30,
+  });
+  // toggle로 on off
+  let toggle = 0;
+  $('.container .footer-bottom-last img').on("click", function() {
+    if (toggle == 0 ) {
+      toggle = 1;
+      $(".container .footer-bottom-last img").css({ transform : "rotate(0)" });
+      $(".container .footer-bottom-toggle").slideDown().css({ display : "block", "margin-top" : "20px" });
 
-      } else {
-        toggle = 0;
-        $(".container .footer-bottom-last img").css({ transform : "rotate(-180deg)" });
-        $(".container .footer-bottom-toggle").slideUp().css({ display : "none" });
-      }
-    });
+    } else {
+      toggle = 0;
+      $(".container .footer-bottom-last img").css({ transform : "rotate(-180deg)" });
+      $(".container .footer-bottom-toggle").slideUp().css({ display : "none" });
+    }
+  });
 });
